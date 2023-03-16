2ac76e513ff001c322bc999d2ffdd83888129678
unknown <subsub1221@gmail.com>
2023-03-16 16:26:56 +0900

    BE_배포dockerfile수정_230316

--- back/app.js
+++ back/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 8081);
 
 app.get('/', (req, res) => {
     res.send('Hello, Express')
