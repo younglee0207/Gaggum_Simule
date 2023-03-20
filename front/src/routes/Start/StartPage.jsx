19dd859ec88a2bb9813f0bada28a0ae36277234a
Hyokiz <janghh97@gmail.com>
2023-03-20 10:58:09 +0900

    FE_front 기본 템플릿 작성_230320

---
+++ front/src/routes/Start/StartPage.jsx
@@ -0,0 +1,9 @@
+const StartPage = () => {
+  return (
+    <div className="StartPage">
+      <h1>가꿈</h1>
+    </div>
+  )
+};
+
+export default StartPage;
---
+++ front/src/routes/plant/PlantListItem.jsx
@@ -0,0 +1,9 @@
+const PlantListItem = () => {
+  return (
+    <div className="MyPlantListItem">
+      <h1>내 식물 리스트 아이템</h1>
+    </div>
+  )
+};
+
+export default PlantListItem;
