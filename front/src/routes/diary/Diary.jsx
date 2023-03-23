2a140ecbc9ea2c939142e4f0af8f198b7e7c852e
yeong <ap0674@naver.com>
2023-03-23 11:12:33 +0900

    FE_일지년월일 연동_230323

--- front/src/routes/diary/Diary.jsx
+++ front/src/routes/diary/Diary.jsx
@@ -10,6 +10,8 @@ import NavBar from "../../components/navbar/NavBar";
 
 import React, { useState } from "react";
 import Modal from "./Modal";
+import ModalMonth from "./ModalMonth";
+
 import PlantModal from "./PlantModal";
 import WriteModal from "./WriteModal";
 import DiaryList from "./DiaryItem";
@@ -85,8 +87,10 @@ const Diary = () => {
   const [isModalOpen, setIsModalOpen] = useState(false); // 모달 창이 열린 상태인지 여부를 관리하는 상태
   const [isPlantModalOpen, setIsPlantModalOpen] = useState(false); // 모달 창이 열린 상태인지 여부를 관리하는 상태
   const [isWriteModalOpen, setIsWriteModalOpen] = useState(false); // 모달 창이 열린 상태인지 여부를 관리하는 상태
+  const [isModalMonthOpen, setIsModalMonthOpen] = useState(false); // 모달 창이 열린 상태인지 여부를 관리하는 상태
 
-  const [modalButtonName, setModalButtonName] = useState("월");
+  const [modalButtonName, setModalButtonName] = useState("");
+  const [modalMonthButtonName, setModalMonthButtonName] = useState("");
   const [modalPlantButtonName, setModalPlantButtonName] = useState("식물이름");
 
   const openModal = () => {
@@ -102,6 +106,15 @@ const Diary = () => {
   const closePlantModal = () => {
     setIsPlantModalOpen(false);
   };
+  const openModalMonth = () => {
+    setIsModalMonthOpen(true);
+  };
+  const closeModalMonth = () => {
+    setIsModalMonthOpen(false);
+  };
+
+
+
 
   const openWriteModal = () => {
     setIsWriteModalOpen(true);
@@ -109,6 +122,15 @@ const Diary = () => {
   const closeWriteModal = () => {
     setIsWriteModalOpen(false);
   };
+
+  // const openModalMonth = () => {
+  //   setIsWriteModalOpen(true);
+  // };
+  // const closeModalMonth = () => {
+  //   setIsWriteModalOpen(false);
+  // };
+
+
 
   const handleItemClick = (itemName) => {
     setModalButtonName(itemName);
@@ -118,8 +140,13 @@ const Diary = () => {
     setModalPlantButtonName(itemName);
     closePlantModal();
   };
-
+  const handleMonthItemClick = (itemName) => {
+    setModalMonthButtonName(itemName);
+    closeModalMonth();
+  };
   const navigate = useNavigate();
+
+  const [loadedDiaries, setLoadedDiaries] = useState([]);
 
   const GetAllDiaries = () => {
     axios
@@ -127,15 +154,59 @@ const Diary = () => {
       // console.log("성공")
       //replace는 뒤로가기 버튼 비활성 이미 양식 제출했으므로
       .then((response) => {
-        // console.log(response);
+        console.log("이거", response.data.data);
         //then 대신에 asynce나 await가능
         alert("일지 작성 성공.");
-        navigate.replace("/diary");
+        // navigate.replace("/diary");
+        setLoadedDiaries(response.data.data);
       })
       .catch((error) => {
         console.log(error);
         alert("작성에 실패하였습니다.");
       });
+  };
+
+  const GetYearDiaries = (year) => {
+    console.log(year)
+    axios
+      .get(`https://j8b310.p.ssafy.io/api/diary/date?diaryDate=${year}`)
+      // console.log("성공")
+      //replace는 뒤로가기 버튼 비활성 이미 양식 제출했으므로
+      .then((response) => {
+        
+        console.log("이거년도", response.data.data);
+        //then 대신에 asynce나 await가능
+        alert("일지 작성 성공.");
+        // navigate.replace("/diary");
+        setLoadedDiaries(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("작성에 실패하였습니다.");
+      });
+  };
+  const GetYearDiaries2 = () => {
+    console.log(modalButtonName)
+    axios
+      .get(`https://j8b310.p.ssafy.io/api/diary/date?diaryDate=2023`)
+      // console.log("성공")
+      //replace는 뒤로가기 버튼 비활성 이미 양식 제출했으므로
+      .then((response) => {
+        console.log("이거년도", response);
+        //then 대신에 asynce나 await가능
+        alert("일지 작성 성공.");
+        // navigate.replace("/diary");
+        setLoadedDiaries(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("작성에 실패하였습니다.");
+      });
+  };
+
+  const check = () => {
+    console.log(modalButtonName)
+    
   };
 
   return (
@@ -144,7 +215,7 @@ const Diary = () => {
         <div style={{ width: "42px" }}></div>
         <div className="diary-item">
           <h1 style={{ margin: 0 }}>나의 식물 일지</h1>
-          <button onClick={GetAllDiaries}>겟</button>
+          {/* <button onClick={check}>겟</button> */}
         </div>
         <div>
           {/* <button
@@ -157,25 +228,30 @@ const Diary = () => {
         </div>
       </div>
 
+
+
       <hr style={{ margin: 0 }} />
 
+
+
+      {/* 모달 관리 */}
       <div className="justify">
         <div>
           <button onClick={openModal} className="diary-button">
-            {modalButtonName}
+            {modalButtonName}년
             <MdKeyboardArrowDown />
           </button>
           {isModalOpen && (
-            <Modal onClose={closeModal} onItemClick={handleItemClick} />
+            <Modal onClose={closeModal} onItemClick={handleItemClick} GetAllDiaries={GetAllDiaries} GetYearDiaries={GetYearDiaries} check={false}/>
           )}
         </div>
         <div>
-          <button onClick={openModal} className="diary-button">
-            {modalButtonName}
+          <button onClick={openModalMonth} className="diary-button">
+            {modalMonthButtonName}월
             <MdKeyboardArrowDown />
           </button>
-          {isModalOpen && (
-            <Modal onClose={closeModal} onItemClick={handleItemClick} />
+          {isModalMonthOpen && (
+            <ModalMonth onClose={closeModalMonth} onItemClick={handleMonthItemClick} GetAllDiaries={GetAllDiaries} GetYearDiaries={GetYearDiaries} check={true}/>
           )}
         </div>
         <div>
@@ -193,17 +269,20 @@ const Diary = () => {
         </div>
       </div>
 
+
+
+      {/* 일지 보이는 곳 */}
       <div className="PlantList content">
-        {dummyData
-          .filter(
-            (item) =>
-              (modalButtonName === "월" ||
-                item.plant_diary_writedate === modalButtonName ||
-                modalButtonName === "전체보기") &&
-              (modalPlantButtonName === item.diary_title ||
-                modalPlantButtonName === "전체보기" ||
-                modalPlantButtonName === "식물이름")
-          )
+        {loadedDiaries
+          // .filter(
+          //   (item) =>
+          //     (modalButtonName === "월" ||
+          //       item.plant_diary_writedate === modalButtonName ||
+          //       modalButtonName === "전체보기") &&
+          //     (modalPlantButtonName === item.diary_title ||
+          //       modalPlantButtonName === "전체보기" ||
+          //       modalPlantButtonName === "식물이름")
+          // )
           .map((item) => (
             <div key={item.diary_number}>
               <div className="MyPlantListItem">
@@ -211,21 +290,22 @@ const Diary = () => {
                   <img
                     className="img-plant"
                     src={item.diary_img}
-                    alt={item.diary_title}
+                    alt="이미지 로딩 실패"
                   />
                 </div>
 
                 <div className="content-div">
                   <p className="plant-name">{item.diary_title}</p>
-                  <p>{item.plant_diary_content}</p>
-                  <p>{item.plant_diary_writedate}</p>
+                  <p>{item.diary_memo
+}</p>
+                  <p>{item.diary_date}</p>
                 </div>
                 <div>
                   <button onClick={openWriteModal} className="modify-button">
                     수정
                   </button>
                   {isWriteModalOpen && (
-                    <WriteModal onClose={closeWriteModal} item={item} />
+                    <WriteModal onClose={closeWriteModal} item={item} GetAllDiaries={GetAllDiaries}/>
                   )}
                 </div>
               </div>
--- front/src/routes/diary/Modal.jsx
+++ front/src/routes/diary/Modal.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import classes from "./Modal.module.scss";
-const Modal = ({ onClose, onItemClick }) => {
-  const handleItemClick = (e) => {
-    onItemClick(e.target.textContent);
+const Modal = ({
+  onClose,
+  onItemClick,
+  GetAllDiaries,
+  GetYearDiaries,
+}) => {
+  const handleItemClick = async (e) => {
+    await onItemClick(e.target.textContent);
+    await GetYearDiaries();
   };
 
+  const handleYearClick = (e) => {
+    GetYearDiaries(e.target.textContent);
+    onItemClick(e.target.textContent);
+
+  };
   return ReactDOM.createPortal(
     <div className={classes.modalOverlay} onClick={onClose}>
       <div className={classes.modalContent}>
-        <h1 onClick={handleItemClick}>전체보기</h1>
-        <h1 onClick={handleItemClick}>1월</h1>
-        <h1 onClick={handleItemClick}>2월</h1>
-        <h1 onClick={handleItemClick}>3월</h1>
-        <h1 onClick={handleItemClick}>4월</h1>
-        <h1 onClick={handleItemClick}>5월</h1>
-        <h1 onClick={handleItemClick}>6월</h1>
-        <h1 onClick={handleItemClick}>7월</h1>
-        <h1 onClick={handleItemClick}>8월</h1>
-        <h1 onClick={handleItemClick}>9월</h1>
-        <h1 onClick={handleItemClick}>10월</h1>
-        <h1 onClick={handleItemClick}>11월</h1>
-        <h1 onClick={handleItemClick}>12월</h1>
+        <h1 onClick={handleItemClick} >전체보기</h1>
+        <h1 onClick={handleYearClick} >2020</h1>
+        <h1 onClick={handleYearClick} >2021</h1>
+        <h1 onClick={handleYearClick} >2022</h1>
+        <h1 onClick={handleYearClick} >2023</h1>
+        <h1 onClick={handleYearClick} >2024</h1>
+
       </div>
     </div>,
     document.getElementById("modal-root")
