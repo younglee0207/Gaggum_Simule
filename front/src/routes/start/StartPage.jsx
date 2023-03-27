f2df13f695419d843595878a1c3f16856ab15c3d
yeong <ap0674@naver.com>
2023-03-27 17:13:39 +0900

    FE_로그인 완료_230327

--- front/src/routes/start/StartPage.jsx
+++ front/src/routes/start/StartPage.jsx
@@ -3,8 +3,8 @@ import loginImg from "../../assets/start/kakao_login_medium_wide.png";
 
 const StartPage = () => {
   const REST_API_KEY = "58acce2e1c5607a9310ef74870273737";
-  const REDIRECT_URI = "http://localhost:3000/auth/kakao/callback";
-  // const REDIRECT_URI = "https://j8b310.p.ssafy.io/auth/kakao/callback";
+  // const REDIRECT_URI = "http://localhost:3000/auth/kakao/callback";
+  const REDIRECT_URI = "https://j8b310.p.ssafy.io/auth/kakao/callback";
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const handleLogin = () => {
