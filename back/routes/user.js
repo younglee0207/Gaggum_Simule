e0ad7ebff4acfa1b7eaef2b47dd1e61290dabdec
unknown <subsub1221@gmail.com>
2023-03-27 13:32:15 +0900

    BE_login_230327

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var router = express.Router();
+var axios = require('axios');
+const router = express.Router();
 const KAKAO_OAUTH_TOKEN_API_URL = "https://kauth.kakao.com/oauth/token"
 const KAKAO_GRANT_TYPE="authorization_code"
 const KAKAO_CLIENT_id="58acce2e1c5607a9310ef74870273737"
@@ -9,7 +10,7 @@ router.get('/kakao/code', function (req, res, next) {
         let code = req.query.code;
         try{
             axios.post(
-                `${KAKAO_OAUTH_TOKEN_API_URL}?grant_type=${grant_type}&client_id=${client_id}&redirect_uri=${redirect_uri}&code=${code}`
+                `${KAKAO_OAUTH_TOKEN_API_URL}?grant_type=${KAKAO_GRANT_TYPE}&client_id=${KAKAO_CLIENT_id}&redirect_uri=${KAKAO_REDIRECT_URL}&code=${code}`
                 , {
                  headers: {
                     'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
@@ -17,7 +18,7 @@ router.get('/kakao/code', function (req, res, next) {
             }).then((result)=>{
                 console.log(result.data['access_token'])
                 // 토큰을 활용한 로직을 적어주면된다.
-    
+                res.json(result.data);
             }).catch(e=> {
                 console.log(e)
                 res.send(e);
