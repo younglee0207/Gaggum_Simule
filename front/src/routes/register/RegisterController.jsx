4b402b63d95ba21d6cd94a4a8833c29360cf378c
이영재 <ap0674@naver.com>
2023-03-29 15:26:50 +0900

    FE_레지스터 키입력 소켓완료_230329

--- front/src/routes/register/RegisterController.jsx
+++ front/src/routes/register/RegisterController.jsx
@@ -1,78 +1,121 @@
-import "./Register.style.scss"
-import { FaArrowAltCircleUp, FaArrowAltCircleDown, FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa"
-import { io } from "socket.io-client";
-import { useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import {
+  FaArrowAltCircleUp,
+  FaArrowAltCircleDown,
+  FaArrowAltCircleLeft,
+  FaArrowAltCircleRight,
+} from 'react-icons/fa';
 
 const RegisterController = ({ socket }) => {
+  const [UpKeyDown, setUpKeyDown] = useState(false);
+  const [DownKeyDown, setDownKeyDown] = useState(false);
+  const [LeftKeyDown, setLeftKeyDown] = useState(false);
+  const [RightKeyDown, setRightKeyDown] = useState(false);
+
+  const logKeyDown = (direction, number) => {
+    console.log(`${direction} Key is down`, number);
+    socket.emit(`go_${direction}`, { name: `go ${direction}`, data: number });
+    
+  };
+
+  const createInterval = (callback) => {
+    return setInterval(callback, 100);
+  };
+
+  const clearAndNullifyInterval = (intervalId) => {
+    clearInterval(intervalId);
+    return null;
+  };
+
+  const [UpIntervalId, setUpIntervalId] = useState(null);
+  const [DownIntervalId, setDownIntervalId] = useState(null);
+  const [LeftIntervalId, setLeftIntervalId] = useState(null);
+  const [RightIntervalId, setRightIntervalId] = useState(null);
 
   useEffect(() => {
-    // 카메라 받기
-    socket.on()
+    setUpIntervalId((prevIntervalId) =>
+      UpKeyDown && prevIntervalId === null
+        ? createInterval(() => logKeyDown('straight',2))
+        : !UpKeyDown && prevIntervalId !== null
+        ? clearAndNullifyInterval(prevIntervalId)
+        : prevIntervalId
+    );
+  }, [UpKeyDown]);
 
-    return () => {
-      // 카메라 종료
-      socket.off()
-    }
-  }, [])
+  useEffect(() => {
+    setDownIntervalId((prevIntervalId) =>
+      DownKeyDown && prevIntervalId === null
+        ? createInterval(() => logKeyDown('back',3))
+        : !DownKeyDown && prevIntervalId !== null
+        ? clearAndNullifyInterval(prevIntervalId)
+        : prevIntervalId
+    );
+  }, [DownKeyDown]);
 
+  useEffect(() => {
+    setLeftIntervalId((prevIntervalId) =>
+      LeftKeyDown && prevIntervalId === null
+        ? createInterval(() => logKeyDown('left',1))
+        : !LeftKeyDown && prevIntervalId !== null
+        ? clearAndNullifyInterval(prevIntervalId)
+        : prevIntervalId
+    );
+  }, [LeftKeyDown]);
 
-
-  const handleUpMouseDown = () => {
-    // 앞쪽으로 이동
-    socket.emit("go_straight", 
-    { name: "go straight", data: 2 })
-  };
-
-  const handleDown = () => {
-    console.log("뒤")
-    // 뒤쪽으로 이동
-    socket.emit("go_back", 
-    { name: "go back", data: 3 })
-  };
-
-  const handleLeft = () => {
-    console.log("왼쪽")
-    // 왼쪽으로 이동
-    socket.emit("go_left", 
-    { name: "go left", data: 1 })
-  };
-
-  const handleRight = () => {
-    console.log("오른쪽")
-    // 오른쪽으로 이동
-    socket.emit("go_right", 
-    { name: "go right", data: 4 })
-  };
+  useEffect(() => {
+    setRightIntervalId((prevIntervalId) =>
+      RightKeyDown && prevIntervalId === null
+        ? createInterval(() => logKeyDown('right',4))
+        : !RightKeyDown && prevIntervalId !== null
+        ? clearAndNullifyInterval(prevIntervalId)
+        : prevIntervalId
+    );
+  }, [RightKeyDown]);
 
   return (
     <div className="RegisterController">
       <div className="controller__up">
-        <FaArrowAltCircleUp 
-          size="100" 
+        <FaArrowAltCircleUp
+          size="100"
           color="#022a17"
-          // onClick={handleUpKeyDown}
-          onMouseOver={handleUpMouseDown}
+          onKeyDown={() => setUpKeyDown(true)}
+          onKeyUp={() => setUpKeyDown(false)}
+          onTouchStart={() => setUpKeyDown(true)}
+          onTouchEnd={() => setUpKeyDown(false)}
+          tabIndex="0"
         />
       </div>
       <div className="controller__down">
-        <FaArrowAltCircleLeft 
-          size="100" 
+        <FaArrowAltCircleLeft
+          size="100"
           color="#022a17"
-          onClick={handleLeft}
+          onKeyDown={() => setLeftKeyDown(true)}
+          onKeyUp={() => setLeftKeyDown(false)}
+          onTouchStart={() => setLeftKeyDown(true)}
+          onTouchEnd={() => setLeftKeyDown(false)}
+          tabIndex="0"
         />
-        <FaArrowAltCircleDown 
-          size="100" 
+        <FaArrowAltCircleDown
+          size="100"
           color="#022a17"
-          onClick={handleDown}
-        />
-        <FaArrowAltCircleRight 
-          size="100" 
+          onKeyDown={() => setDownKeyDown(true)}
+          onKeyUp={() => setDownKeyDown(false)}
+          onTouchStart={() => setDownKeyDown(true)}
+          onTouchEnd={() => setDownKeyDown(false)}
+          tabIndex="0"
+          />
+          <FaArrowAltCircleRight
+          size="100"
           color="#022a17"
-          onClick={handleRight}
-        />
-      </div>
-    </div>
-  )
-};
-
-export default RegisterController;
+          onKeyDown={() => setRightKeyDown(true)}
+          onKeyUp={() => setRightKeyDown(false)}
+          onTouchStart={() => setRightKeyDown(true)}
+          onTouchEnd={() => setRightKeyDown(false)}
+          tabIndex="0"
+          />
+          </div>
+          </div>
+          );
+          };
+          
+          export default RegisterController;
