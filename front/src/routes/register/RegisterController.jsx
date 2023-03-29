bfc5ce12460037c09387cffa15f250f14d07d6de
윤동민 <kkang15794@naver.com>
2023-03-29 14:06:24 +0900

    ROS2_move turtleBot_230329

--- front/src/routes/register/RegisterController.jsx
+++ front/src/routes/register/RegisterController.jsx
@@ -1,72 +1,64 @@
-import "./Register.style.scss"
-import { FaArrowAltCircleUp, FaArrowAltCircleDown, FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa"
+import "./Register.style.scss";
+import {
+  FaArrowAltCircleUp,
+  FaArrowAltCircleDown,
+  FaArrowAltCircleLeft,
+  FaArrowAltCircleRight,
+} from "react-icons/fa";
 import { io } from "socket.io-client";
 import { useEffect } from "react";
 
 const RegisterController = ({ socket }) => {
-
   useEffect(() => {
     // 카메라 받기
-    socket.on()
+    socket.on();
 
     return () => {
       // 카메라 종료
-      socket.off()
-    }
-  }, [])
+      socket.off();
+    };
+  }, []);
 
   const handleUp = () => {
-    console.log("위")
+    console.log("위");
     // 앞쪽으로 이동
-    socket.emit("go_straight", { data: "go straight" })
+    socket.emit("go_straight", { data: "go straight" });
   };
 
   const handleDown = () => {
-    console.log("아래")
+    console.log("아래");
     // 뒤쪽으로 이동
-    socket.emit("go_back", { data: "go back" })
+    socket.emit("go_back", { data: "go back" });
   };
 
   const handleLeft = () => {
-    console.log("왼쪽")
+    console.log("왼쪽");
     // 왼쪽으로 이동
-    socket.emit("go_left", { data: "go left" })
+    socket.emit("go_left", { data: "go left" });
   };
 
   const handleRight = () => {
-    console.log("오른쪽")
+    console.log("오른쪽");
     // 오른쪽으로 이동
-    socket.emit("go_right", { data: "go right" })
+    socket.emit("go_right", { data: "go right" });
   };
 
   return (
     <div className="RegisterController">
       <div className="controller__up">
-        <FaArrowAltCircleUp 
-          size="100" 
-          color="#022a17"
-          onClick={handleUp}
-        />
+        <FaArrowAltCircleUp size="100" color="#022a17" onClick={handleUp} />
       </div>
       <div className="controller__down">
-        <FaArrowAltCircleLeft 
-          size="100" 
-          color="#022a17"
-          onClick={handleLeft}
-        />
-        <FaArrowAltCircleDown 
-          size="100" 
-          color="#022a17"
-          onClick={handleDown}
-        />
-        <FaArrowAltCircleRight 
-          size="100" 
+        <FaArrowAltCircleLeft size="100" color="#022a17" onClick={handleLeft} />
+        <FaArrowAltCircleDown size="100" color="#022a17" onClick={handleDown} />
+        <FaArrowAltCircleRight
+          size="100"
           color="#022a17"
           onClick={handleRight}
         />
       </div>
     </div>
-  )
+  );
 };
 
 export default RegisterController;
