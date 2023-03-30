c15b7d44f963941c1a8fcb9f90eae8afd3dca09c
윤동민 <kkang15794@naver.com>
2023-03-30 09:47:56 +0900

    BE_aws S3 코드 추가_230330

--- back/aws/s3.js
+++ back/aws/s3.js
@@ -1,31 +1,34 @@
-// const fs = require("fs");
-// const AWS = require("aws-sdk");
-// const dotenv = require("dotenv");
-// const config = require("../config");
+const fs = require("fs");
+const AWS = require("aws-sdk");
+const dotenv = require("dotenv");
+const config = require("../config");
 
-// dotenv.config();
+dotenv.config();
 
-// const s3 = new AWS.S3({
-//   accessKeyId: config.awsKey.accessKey,
-//   secretAccessKey: config.awsKey.secretAccessKey,
-// });
+const s3 = new AWS.S3({
+  accessKeyId: config.awsKey.accessKey,
+  secretAccessKey: config.awsKey.secretAccessKey,
+});
 
-// const uploadFile = () => {
-//   const params = {
-//     Bucket: "ssafybucket",
-//     Key: "image/" + "imageTest",
-//     Body: fs.readFileSync("pot.jpg"),
-//     ContentType: "image/png",
-//   };
+const uploadFile = (name, base64) => {
+  //const base64Data = new Buffer.from(imgfile.re)
+  const base64Data = new Buffer.from(base64.replace("", ""), "base64");
+  const type = base64.split(";")[0].split("/")[1];
+  const params = {
+    Bucket: "ssafybucket",
+    Key: "image/" + name,
+    Body: base64Data,
+    ContentType: `image/${type}`,
+  };
 
-//   s3.upload(params, function (err, data) {
-//     if (err) {
-//       throw err;
-//     }
-//     console.log(`File uploaded successfully. ${data}`);
-//   });
-// };
+  s3.upload(params, function (err, data) {
+    if (err) {
+      throw err;
+    }
+    console.log(`File uploaded successfully. ${data}`);
+  });
+};
 
-// module.exports = {
-//   uploadFile,
-// };
+module.exports = {
+  uploadFile,
+};
