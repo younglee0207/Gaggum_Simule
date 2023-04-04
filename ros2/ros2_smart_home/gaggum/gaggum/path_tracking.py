b0a3607b9c69bc5df103dd43db7ba7841304454b
윤동민 <kkang15794@naver.com>
2023-04-05 08:41:32 +0900

    ROS_path_tracking 소켓주소 변경_230405

--- ros2/ros2_smart_home/gaggum/gaggum/path_tracking.py
+++ ros2/ros2_smart_home/gaggum/gaggum/path_tracking.py
@@ -65,8 +65,8 @@ def auto_move(data):
 
     print("auto_move", data)    
 
-# ip_server = "https://j8b310.p.ssafy.io/socket"
-ip_server = 'http://localhost:3001'
+ip_server = "https://j8b310.p.ssafy.io/socket"
+# ip_server = 'http://localhost:3001'
 
 print("connect ", ip_server)
 sio.connect(ip_server)
