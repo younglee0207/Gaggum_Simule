964b340cf9e51add9af9c0ab9bf0832d6f87fbb5
장현혁 <janghh97@gmail.com>
2023-04-04 12:25:18 +0900

    FE_카메라 부분 수정_230404

--- front/src/routes/register/RegisterCamera.jsx
+++ front/src/routes/register/RegisterCamera.jsx
@@ -4,10 +4,10 @@ import { useState, useEffect } from "react";
 import noCameraImg from "../../assets/register/Camera-not-Working.png"
 
 const RegisterCamera = ({ socket }) => {
-  // 들어가는 정보 : stream_image, plant_x, plant_y, plant_name
+  // 들어가는 정보 : plant_img, plant_position_x, plant_position_y, plant_original_name
   const [camera, setCamera] = useState(false);
 
-  const cameraSource = camera.stream_image ? `data:image/jpeg;base64,${camera.stream_image}` : noCameraImg
+  const cameraSource = camera.plant_img ? `data:image/jpeg;base64,${camera.plant_img}` : noCameraImg
 
   useEffect(() => {
     socket.on("streaming_image", (data) => {
@@ -58,7 +58,7 @@ const RegisterCamera = ({ socket }) => {
           alt=""
           />
       </div>
-      {camera.stream_image ? (
+      {camera.plant_img ? (
         <h4 className="camera__title">화분을 화면 중앙에 맞춰주세요</h4>
       ) : (
         <h4 className="camera__title">연결된 카메라가 없습니다.</h4>
